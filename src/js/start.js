526e93ccc44bc3dd0f4741da02004a32a141789b
Omar Alhamad <omar93@hotmail.se>
2017-12-01 19:19:14 +0100

    need to add q1

--- src/js/start.js
+++ src/js/start.js
@@ -1,8 +1,9 @@
 let start = document.querySelector('#start')
+let gameWindow = document.querySelector('#quizz')
+let welcomeWindow = document.querySelector('#welcome')
+
 start.addEventListener('click', function () {
-  let gameWindow = document.querySelector('#quizz')
-  let welcomeWindow = document.querySelector('#welcome')
   welcomeWindow.classList.add('hide')
   gameWindow.classList.remove('hide')
-  console.log('apa')
-}, true)
+  document.querySelector('#question').textContent = 'Q1' // add the first question from server
+})
